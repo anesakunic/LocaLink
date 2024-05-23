da356ae35d7e5256a7773d5fba480ab4c1fb0b1d
Nur <nurrustem@gmail.com>
2024-05-23 14:42:08 +0200

    Responsive homepage

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,46 +4,45 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="bg-blue text-font flex font-urbanist">
-      <header className="w-full max-w-6xl mx-auto flex p-4">
-        <svg 
-          width="37"
-          height="37"
-          viewBox="0 0 37 37"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M33.3112 7.94355C30.0614 3.20234 24.6056 0.0920105 18.4233 0.0920105C8.46189 0.0920105 0.386536 8.16736 0.386536 18.1288C0.386536 19.5781 0.557464 20.9874 0.880246 22.3377L10.0653 13.1527C10.9458 12.2722 12.3733 12.2722 13.2538 13.1527L20.6779 20.5768L33.3112 7.94355Z"
-            fill="#EF2A82"
-          />
-          <path
-            d="M35.455 12.1768L22.2722 25.3596C21.3917 26.24 19.9642 26.24 19.0837 25.3596L11.6595 17.9354L2.67196 26.923C5.75744 32.4376 11.655 36.1656 18.4233 36.1656C28.3848 36.1656 36.4602 28.0903 36.4602 18.1288C36.4602 16.0432 36.1061 14.0402 35.455 12.1768Z"
-            fill="#EF2A82"
-          />
-        </svg>
-
-        <div className="text-2xl font-bold uppercase mx-5 mt-1">LocaLink</div>
-        <nav className="flex space-x-14 ml-72 text-nav p-2">
-          
-        <Link to="/" className="hover:text-pink">
+    <div className="bg-blue text-font flex font-urbanist w-full">
+      <header className="w-full max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4">
+        <div className="flex items-center">
+          <svg 
+            width="37"
+            height="37"
+            viewBox="0 0 37 37"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M33.3112 7.94355C30.0614 3.20234 24.6056 0.0920105 18.4233 0.0920105C8.46189 0.0920105 0.386536 8.16736 0.386536 18.1288C0.386536 19.5781 0.557464 20.9874 0.880246 22.3377L10.0653 13.1527C10.9458 12.2722 12.3733 12.2722 13.2538 13.1527L20.6779 20.5768L33.3112 7.94355Z"
+              fill="#EF2A82"
+            />
+            <path
+              d="M35.455 12.1768L22.2722 25.3596C21.3917 26.24 19.9642 26.24 19.0837 25.3596L11.6595 17.9354L2.67196 26.923C5.75744 32.4376 11.655 36.1656 18.4233 36.1656C28.3848 36.1656 36.4602 28.0903 36.4602 18.1288C36.4602 16.0432 36.1061 14.0402 35.455 12.1768Z"
+              fill="#EF2A82"
+            />
+          </svg>
+          <div className="text-2xl font-bold uppercase mx-3 mt-1">LocaLink</div>
+        </div>
+        <nav className="flex flex-col sm:flex-row sm:items-center sm:space-x-10 text-nav">
+          <Link to="/" className="hover:text-pink mb-2 sm:mb-0">
             Home
           </Link>
-          <Link to="/feed" className="hover:text-pink">
-            Reports
+          <Link to="/feed" className="hover:text-pink mb-2 sm:mb-0">
+            Feed
           </Link>
-          <a href="#instructions" className="hover:text-pink">
+          <a href="#instructions" className="hover:text-pink mb-2 sm:mb-0">
             Instructions
           </a>
-          <a href="#faq" className="hover:text-pink">
+          <a href="#faq" className="hover:text-pink mb-2 sm:mb-0">
             FAQ
           </a>
-        
-          <div className="space-x-4">
+          <div className="flex items-center space-x-4">
             <Link to="/signin" className="hover:text-pink">
               Sign in
             </Link>
-            <Link to="/signup" className="bg-transparent hover:bg-pink hover:text-blue rounded-2xl border-solid border-2 border-nav px-8 py-1">
+            <Link to="/signup" className="bg-transparent hover:bg-pink hover:text-blue rounded-2xl border-solid border-2 border-nav px-4 py-1">
               Sign Up
             </Link>
           </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -9,6 +9,9 @@ import AddLocation from "../assets/addlocation.png";
 import Outcome from "../assets/outcome.png";
 import RepOne from "../assets/reportone.png";
 import RepTwo from "../assets/reporttwo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 const reportData = {
   imageReportOne: RepOne,
@@ -43,66 +46,82 @@ const HomePage = () => {
   return (
     <div>
       <Header /> {/* Use the Header component here */}
-      <hero>
-        <div className="bg-blue h-[520px] font-urbanist">
-          <div className="p-32 w-[690px] mx-20">
-            <h1 className="text-5xl text-font font-semibold">
-              Citizens for a More Orderly Future
-            </h1>
-            <p className="text-nav w-[500px] mt-5">
-              Welcome to LocalLink - an innovative platform that enables
-              citizens to actively contribute to the improvement and cleanliness
-              of our city. Your concern for communal problems can now become a
-              force for positive change
-            </p>
-            <button onClick={handleReportClick} className="bg-pink px-20 py-1 text-font text-2xl rounded-full mt-10">
-              Report a Problem
-            </button>
-          </div>
+      <div className="bg-blue h-[520px] font-urbanist">
+        <div className="p-8 md:p-32 w-full md:w-[690px] mx-5 md:mx-20">
+          <h1 className="text-3xl md:text-5xl text-font font-semibold">
+            Citizens for a More Orderly Future
+          </h1>
+          <p className="text-nav mt-5">
+            Welcome to LocaLink - an innovative platform that enables
+            citizens to actively contribute to the improvement and cleanliness
+            of our city. Your concern for communal problems can now become a
+            force for positive change
+          </p>
+          <button onClick={handleReportClick} className="bg-pink px-10 py-2 md:px-20 md:py-1 text-font text-2xl md:text-3xl rounded-full mt-10">
+            Report a Problem
+          </button>
         </div>
-      </hero>
-
-      <div className="bg-pink w-full h-5"></div>
-      <div className="">
-        <h1 className="text-4xl text-blue font-semibold font-urbanist justify-center flex mt-6">
+      </div>
+      <div className="bg-pink w-full h-5" />
+      <div id="instructions">
+        <h1 className="text-4xl text-blue font-semibold font-urbanist text-center mt-6">
           Simply create an application and track its outcome
         </h1>
-        <div className="flex justify-center space-x-5 p-10 font-urbanist">
+        <div className="flex flex-wrap justify-center space-x-5 p-10 font-urbanist">
           <Card image={cardData.imageImage} title={cardData.titleImage} description={cardData.descriptionImage} />
           <Card image={cardData.imageLocation} title={cardData.titleLocation} description={cardData.descriptionLocation} />
           <Card image={cardData.imageDetails} title={cardData.titleDetails} description={cardData.descriptionDetails} />
           <Card image={cardData.imageOutcome} title={cardData.titleOutcome} description={cardData.descriptionOutcome} />
         </div>
       </div>
-      <div className="font-urbanist text-center mt-10">
-        <h1 className="text-blue text-4xl font-semibold">Community Issues & Solutions Blog</h1>
-        <p className="text-nav w-[1000px] p-10 ml-60">Welcome to the Community Issues & Solutions Blog! Here, you'll find real stories and problems shared by our community members, along with the creative solutions and support they've received. Join the conversation, share your own experiences, and connect with others who are making a difference in our community.</p>
-        <div className="ml-10 flex justify-center space-x-5">
+      <div id="reports" className="font-urbanist text-center mt-10">
+        <h1 className="text-blue text-4xl font-semibold">Community Issues & Solutions Feed</h1>
+        <p className="text-nav w-full md:w-[1000px] p-5 md:p-10 mx-auto">Welcome to the Community Issues & Solutions Feed! Here, you'll find real stories and problems shared by our community members, along with the creative solutions and support they've received. Join the conversation, share your own experiences, and connect with others who are making a difference in our community.</p>
+        <div className="flex flex-wrap justify-center space-x-5">
           <Card image={reportData.imageReportOne} />
           <Card image={reportData.imageReportTwo} />
         </div>
       </div>
-      <div>
-        <h1 className="text-blue font-urbanist text-4xl font-semibold text-center mt-10">FAQ</h1>
-      </div>
-      <footer className="bg-blue h-[200px]">
-        <div className="flex justify-center ml-60 space-x-10 text-font w-[1150px]">
-          <div>
-            <h1 className="mt-5">How do I change my account email?</h1>
+      {/* FAQ Section */}
+      <section id="faq" className="bg-blue p-10">
+        <h1 className="text-font text-4xl font-semibold text-center">FAQ</h1>
+        <div className="flex flex-wrap justify-center md:justify-between text-font w-full md:w-[1150px] mx-auto space-y-4 md:space-y-0">
+          <div className="w-full md:w-1/3 p-2">
+            <h2 className="mt-5">How do I change my account email?</h2>
             <p className="mt-2">You can log in to your account and change it from your Profile Edit Profile. Then go to the general tab to change your email.</p>
           </div>
-          <div>
-            <h1 className="mt-5">How can I report an issue or submit a suggestion?</h1>
+          <div className="w-full md:w-1/3 p-2">
+            <h2 className="mt-5">How can I report an issue or submit a suggestion?</h2>
             <p className="mt-2">To report an issue or submit a suggestion, simply click on the "Report a Problem" button within the Localink platform. Follow the prompted steps to provide details about the problem or suggestion you wish to share. Our team will review your submission and take appropriate action accordingly.</p>
           </div>
-          <div>
-            <h1 className="mt-5">Is Localink available in my area?</h1>
+          <div className="w-full md:w-1/3 p-2">
+            <h2 className="mt-5">Is Localink available in my area?</h2>
             <p className="mt-2">Yes, Localink is available for the entire country of Bosnia and Herzegovina. Whether you're in urban centers or rural communities, you can access Localink to connect with your neighbors, stay informed about local events, and engage with your community.</p>
           </div>
         </div>
+      </section>
+      
+      <footer id="footer" className="bg-blue h-[200px] flex justify-center items-center">
+        <button onClick={() => window.scrollTo(0, 0)} className="text-font hover:text-gray-300 mr-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className="h-8 w-8"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+          </svg>
+        </button>
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="text-font hover:text-gray-300 mx-6">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="mailto:contact@localink.com" className="text-font hover:text-gray-300 ml-6">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </a>
       </footer>
     </div>
   );
 };
 
 export default HomePage;
+
